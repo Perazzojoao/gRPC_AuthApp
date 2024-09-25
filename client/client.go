d18f4293be28e0dc917194daeb87e99cd9b822e2
Perazzojoao <perazzojoao@gmail.com>
2024-09-25 00:37:15 -0300

    feat: adding a helper function to setup a testing gRPC connection

--- client/client.go
+++ client/client.go
@@ -1,8 +1,10 @@
 package client
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"google.golang.org/grpc"
@@ -13,6 +15,17 @@ func NewGrpcConnection() *grpc.ClientConn {
 	serverUrl := fmt.Sprintf("localhost:%s", os.Getenv("GRPC_PORT"))
 
 	conn, err := grpc.NewClient(serverUrl, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("could not connect to server: %v", err)
+	}
+
+	return conn
+}
+
+func NewTestingGrpcConnection(dialer func(context.Context, string) (net.Conn, error)) *grpc.ClientConn {
+	serverUrl := fmt.Sprintf("localhost:%s", os.Getenv("GRPC_PORT"))
+
+	conn, err := grpc.NewClient(serverUrl, grpc.WithContextDialer(dialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("could not connect to server: %v", err)
 	}
